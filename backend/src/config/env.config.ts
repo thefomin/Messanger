061fa5192fe6d6ffc80952bc666fa4ae4b023198
thefomin <raveyt@yandex.ru>
2026-03-04 09:28:03 +0100

    chore: added deps of config

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 
 export const env = {
-  REDIS_URL: process.env.REDIS_URI!,
-  NODE_ENV: process.env.NODE_ENV ?? "development"
+  REDIS_URI: process.env.REDIS_URI,
+  NODE_ENV: process.env.NODE_ENV ?? "development",
+  JWT_SECRET: process.env.JWT_SECRET!,
+  PORT: process.env.HTTP_PORT || 3000,
+  HOST: process.env.HTTP_HOST!
 };
--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -1,5 +1,5 @@
 import Redis from "ioredis";
 import { env } from ".";
 
+export const redis = new Redis(env.REDIS_URI);
 
-export const redis = new Redis(env.REDIS_URL);
