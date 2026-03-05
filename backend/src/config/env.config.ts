84766266b95e9e36c218458db876425926ed92ff
thefomin <raveyt@yandex.ru>
2026-03-05 22:38:40 +0700

    refactor: added env

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -1,9 +1,10 @@
-import "dotenv/config";
+import "dotenv/config"
 
 export const env = {
-  REDIS_URI: process.env.REDIS_URI,
-  NODE_ENV: process.env.NODE_ENV ?? "development",
-  JWT_SECRET: process.env.JWT_SECRET!,
-  PORT: process.env.HTTP_PORT || 3000,
-  HOST: process.env.HTTP_HOST!
-};
+	REDIS_URI: process.env.REDIS_URI,
+	NODE_ENV: process.env.NODE_ENV ?? "development",
+	JWT_SECRET: process.env.JWT_SECRET!,
+	PORT: process.env.HTTP_PORT || 3000,
+	HOST: process.env.HTTP_HOST!,
+	DATABASE_URI: process.env.DATABASE_URI!,
+}
