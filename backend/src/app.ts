0cbc408010cbfcc56c2da3b6054451c105eeba6d
thefomin <raveyt@yandex.ru>
2026-03-06 23:55:07 +0700

    chore: added fastify cookie register

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
 import Fastify from "fastify"
 import websocket from "@fastify/websocket"
 import { ApiModule } from "./api/api.module"
+import fastifyCookie from "@fastify/cookie"
 
 export const app = Fastify({ logger: true })
 
 app.register(websocket)
-
+app.register(fastifyCookie, {
+	secret: process.env.COOKIE_SECRET || "default_cookie_secret",
+	parseOptions: {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === "production",
+		sameSite: "strict",
+	},
+})
 ApiModule(app)
