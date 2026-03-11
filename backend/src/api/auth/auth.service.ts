7fd7262fdcfb7ce75b0d076b049a5bb897e0cbc1
thefomin <raveyt@yandex.ru>
2026-03-11 08:28:19 +0700

    refactor(backend): changed logic register

--- backend/src/api/auth/auth.service.ts
+++ backend/src/api/auth/auth.service.ts
@@ -26,9 +26,22 @@ export class AuthService {
 
 		const hash = await argon2.hash(password)
 
-		return PrismaService.user.create({
+		// создаём пользователя
+		const user = await PrismaService.user.create({
 			data: { email, username, password: hash },
 		})
+
+		// создаём сессию
+		const { sessionId, refreshToken } = await SessionService.create(user.id)
+
+		// создаём accessToken
+		const accessToken = jwt.sign(
+			{ userId: user.id, sessionId },
+			env.JWT_SECRET,
+			{ expiresIn: "15m" },
+		)
+
+		return { accessToken, refreshToken, user }
 	}
 
 	public async login(email: string, password: string) {
@@ -36,25 +49,27 @@ export class AuthService {
 			where: { email },
 		})
 		if (!existingUser) throw new HttpStatus(401, "Invalid email or password")
+
 		const valid = await argon2.verify(existingUser.password, password)
 		if (!valid) throw new HttpStatus(401, "Invalid password")
 
-		const sessionId = await SessionService.create(existingUser.id)
-		const user = await PrismaService.user.findUnique({
-			where: { id: existingUser.id },
-			select: { id: true, email: true, username: true, publicKey: true },
-		})
+		// создаём сессию
+		const { sessionId, refreshToken } = await SessionService.create(
+			existingUser.id,
+		)
+
+		// создаём accessToken
 		const accessToken = jwt.sign(
 			{ userId: existingUser.id, sessionId },
 			env.JWT_SECRET,
 			{ expiresIn: "15m" },
 		)
 
-		const refreshToken = jwt.sign(
-			{ userId: user?.id, sessionId },
-			env.JWT_REFRESH_SECRET,
-			{ expiresIn: "7d" },
-		)
+		const user = await PrismaService.user.findUnique({
+			where: { id: existingUser.id },
+			select: { id: true, email: true, username: true, publicKey: true },
+		})
+
 		return { accessToken, refreshToken, user }
 	}
 
@@ -62,23 +77,22 @@ export class AuthService {
 		await SessionService.delete(sessionId)
 	}
 
-	public async refresh(refreshToken: string) {
-		const payload = jwt.verify(
-			refreshToken,
-			env.JWT_REFRESH_SECRET,
-		) as RefreshTokenPayload
+	public async refresh(oldRefreshToken: string) {
+		const session = await SessionService.getByRefreshToken(oldRefreshToken)
+		console.log("session " + JSON.stringify(session))
+		console.log("oldRefreshToken " + oldRefreshToken)
+		if (!session) throw new HttpStatus(401, "Invalid session or refresh token")
 
-		const session = await SessionService.get(payload.sessionId)
-		if (!session) {
-			throw new HttpStatus(401, "Invalid session")
-		}
-
+		// новый accessToken
 		const accessToken = jwt.sign(
-			{ userId: payload.userId, sessionId: payload.sessionId },
+			{ userId: session.userId, sessionId: session.sessionId },
 			env.JWT_SECRET,
 			{ expiresIn: "15m" },
 		)
 
-		return { accessToken }
+		// новый refreshToken (rotation)
+		const refreshToken = await SessionService.rotate(session.sessionId)
+		console.log("newRefreshToken " + refreshToken)
+		return { accessToken, refreshToken }
 	}
 }
