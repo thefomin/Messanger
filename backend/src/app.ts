f7d2d9eb4040269125b4c0e926e0afd404cb911f
thefomin <raveyt@yandex.ru>
2026-03-11 14:48:34 +0700

    refactor(backend): added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,16 +2,21 @@ import Fastify from "fastify"
 import websocket from "@fastify/websocket"
 import { ApiModule } from "./api/api.module"
 import fastifyCookie from "@fastify/cookie"
+import fastifyCors from "@fastify/cors"
 
 export const app = Fastify({ logger: true })
 
 app.register(websocket)
+app.register(fastifyCors, {
+	origin: "http://localhost:5173",
+	credentials: true,
+})
 app.register(fastifyCookie, {
 	secret: process.env.COOKIE_SECRET || "default_cookie_secret",
 	parseOptions: {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
+		sameSite: "lax",
 	},
 })
 ApiModule(app)
