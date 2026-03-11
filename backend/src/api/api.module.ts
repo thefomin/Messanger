9f076743b95330c99b69b37f8931b14c5b5ebc33
thefomin <raveyt@yandex.ru>
2026-03-11 08:30:19 +0700

    refactor(backend): changed paths api module

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -1,12 +1,10 @@
 import { FastifyInstance } from "fastify"
-import { authModule } from "@/api/auth/auth.module"
-import { usersModule } from "./users/users.module"
-import { chatsModule } from "./chats/chats.module"
+import { AuthModule } from "@/api/auth/auth.module"
+import { WebSocketModule } from "./websocket/websocket.module"
 
 export function ApiModule(app: FastifyInstance) {
-	app.register(authModule, { prefix: "/api/v1/auth" })
-	app.register(usersModule, { prefix: "/api/v1/users" })
-	app.register(chatsModule, { prefix: "/api/v1/" })
+	app.register(AuthModule, { prefix: "/api/v1/auth" })
+	app.register(WebSocketModule, { prefix: "/api/v1/" })
 	app.register(async (app) => {
 		app.get("/health", async () => {
 			return { status: "ok" }
