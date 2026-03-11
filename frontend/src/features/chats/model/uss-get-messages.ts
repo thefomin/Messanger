edc1c06fce0f78bbb856248988bf64937aa692b7
thefomin <raveyt@yandex.ru>
2026-03-11 14:56:54 +0700

    feat: added use get messages

---
+++ frontend/src/features/chats/model/uss-get-messages.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query"
+import { MessageDto } from "./websocket.types"
+
+export const useGetMessages = ({ chatId }: { chatId: string }) => {
+	const { data: messages = [] } = useQuery<MessageDto[]>({
+		queryKey: ["chatMessages", chatId],
+		enabled: !!chatId,
+		queryFn: () => Promise.resolve([]), // заглушка — данные уже в кэше
+		gcTime: Infinity, // не удаляем из кэша (опционально)
+	})
+
+	return { messages }
+}
