0642f0ff5c40980c9309966f93c37e3e46a271e9
thefomin <raveyt@yandex.ru>
2026-03-11 08:45:32 +0700

    feat(frontend): added ui auth

--- frontend/src/features/auth/pages/login.page.tsx
+++ frontend/src/features/auth/pages/login.page.tsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom"
+import { AuthLayout, LoginForm } from "../ui"
+import { ROUTES } from "@/shared/model"
+
 export function LoginPage() {
-	return <div>LoginPage</div>
+	return (
+		<AuthLayout
+			title="вход"
+			form={<LoginForm />}
+			description={
+				<>
+					<Link to={ROUTES.REGISTER} className="text-lg text-[#41A1E6]">
+						или создать аккаунт
+					</Link>
+				</>
+			}
+			footer={
+				<Link
+					to={ROUTES.RESET_PASSWORD}
+					className="text-foreground/50 w-full text-center"
+				>
+					забыли пароль?
+				</Link>
+			}
+		/>
+	)
 }
 
 export const Component = LoginPage
--- frontend/src/features/auth/pages/register.page.tsx
+++ frontend/src/features/auth/pages/register.page.tsx
@@ -1,5 +1,21 @@
+import { ROUTES } from "@/shared/model"
+import { Link } from "react-router-dom"
+import { AuthLayout, RegisterForm } from "../ui"
+
 export function RegisterPage() {
-	return <div>RegisterPage</div>
+	return (
+		<AuthLayout
+			title="регистрация"
+			description={
+				<>
+					<Link to={ROUTES.LOGIN} className="text-lg text-[#41A1E6]">
+						или войти в аккаунт
+					</Link>
+				</>
+			}
+			form={<RegisterForm />}
+		/>
+	)
 }
 
 export const Component = RegisterPage
